2949a6c730d4394e45ec42f97f6e41c38db2d31f
Chanti <chvenkatesh.1118@gmail.com>
2021-11-01 20:36:32 +0530

    4

--- creatinstance.sh
+++ creatinstance.sh
@@ -7,13 +7,15 @@ CREATE() {
     aws ec2 run-instances --launch-template LaunchTemplateId=lt-03d452b52cee20ce4,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
-    UPDATE_DNS_RECORDS $1
     return
   fi
 
   sleep 5
 
-  UPDATE_DNS_RECORDS $1
+  IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+  ## xargs is used to remove the double  quotes
+  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq  &>/dev/null
 }
 
 if [ "$1" == "all" ]; then

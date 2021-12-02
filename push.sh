9160e9cf101fcd9d43112208b727be08ac67523d
Chanti <chvenkatesh.1118@gmail.com>
2021-12-02 07:39:30 +0530

    auto pushing chainges repo

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+pwd && cd $(ls -lt | awk 'NR==2{print$NF}')
+##cd $(ls -lt | awk 'NR==2{print$NF}')
 git add .
-git commit -A
-git commit -m "commit"
+git commit -m "auto pushing chainges repo"
 git push
-
-
